8b1e3bfddcf9dc1dc9a41693f4bc71df89a71847
Donald Sawyer <donald.sawyer@gmail.com>
2023-08-06 14:33:34 -0600

    fixed cases where name is null or whitespace

--- dbt_test_demo/models/gold/gld_carrier.sql
+++ dbt_test_demo/models/gold/gld_carrier.sql
@@ -2,10 +2,11 @@
 
 SELECT
     code,
-    name,
+    TRIM(COALESCE(name, '')) AS name,
     COALESCE(effectiveFromYear, 1900) AS effectiveFromYear,
     COALESCE(effectiveToYear, 9999) AS effectiveToYear
 FROM {{ ref('slv_carrier')}}
 WHERE
     COALESCE(effectiveFromYear, 1900) > 0
     AND COALESCE(effectiveToYear, 9999) > 0
+    AND TRIM(COALESCE(name, '')) != ''
