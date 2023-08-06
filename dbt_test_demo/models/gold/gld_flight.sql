7ddc5dfda7c44f0693e99a201885103789fe6744
Donald Sawyer <donald.sawyer@gmail.com>
2023-08-06 15:40:56 -0600

    gold.flight: added month column, including data and unit tests.

--- dbt_test_demo/models/gold/gld_flight.sql
+++ dbt_test_demo/models/gold/gld_flight.sql
@@ -1,7 +1,9 @@
 {{ config(materialized='view', alias='flight')}}
 
 SELECT
-    year AS year
+    year AS year,
+    month AS month
 FROM {{ ref('slv_flight')}}
 WHERE
     year >= 1900
+    AND month BETWEEN 1 AND 12
--- dbt_test_demo/tests/unit/gold/test_gld_flight.sql
+++ dbt_test_demo/tests/unit/gold/test_gld_flight.sql
@@ -7,16 +7,18 @@
 {% call dbt_unit_testing.test('gld_flight', 'validates transformation logic for gold.flight') %}
 
     {% call dbt_unit_testing.mock_ref('slv_flight') %}
-        YEAR 
-        2000
-        1900
-        1899
-        0
+        YEAR    | MONTH 
+        2000    | 1
+        2000    | 0
+        2000    | 13
+        1900    | 12
+        1899    | NULL
+        0       | NULL
     {% endcall %}
 
     {% call dbt_unit_testing.expect() %}
-        YEAR
-        2000
-        1900
+        YEAR    | MONTH
+        2000    | 1
+        1900    | 12
     {% endcall %}
 {% endcall %}
