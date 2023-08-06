c927e2431f27299cac4298000f44c1364ea5cf6f
Donald Sawyer <donald.sawyer@gmail.com>
2023-08-06 16:16:14 -0600

    gold.flight: added dayOfMonth column, including data and unit tests.

--- dbt_test_demo/models/gold/gld_flight.sql
+++ dbt_test_demo/models/gold/gld_flight.sql
@@ -1,9 +1,24 @@
 {{ config(materialized='view', alias='flight')}}
 
+WITH flightsWithDataLimitCTE AS (
+    SELECT year,
+        month,
+        dayOfMonth,
+        CASE 
+            WHEN month IN (1, 3, 5, 7, 8, 10, 12) THEN 31
+            WHEN month IN (4, 6, 9, 11) THEN 30
+            WHEN month = 2 THEN 29
+            ELSE NULL
+        END AS maxDaysInMonth
+    FROM {{ ref('slv_flight')}}
+)
+
 SELECT
     year AS year,
-    month AS month
-FROM {{ ref('slv_flight')}}
+    month AS month,
+    dayOfMonth AS dayOfMonth
+FROM flightsWithDataLimitCTE
 WHERE
     year >= 1900
     AND month BETWEEN 1 AND 12
+    AND (dayOfMonth > 0 AND dayOfMonth <= maxDaysInMonth)
--- dbt_test_demo/tests/unit/gold/test_gld_flight.sql
+++ dbt_test_demo/tests/unit/gold/test_gld_flight.sql
@@ -7,18 +7,21 @@
 {% call dbt_unit_testing.test('gld_flight', 'validates transformation logic for gold.flight') %}
 
     {% call dbt_unit_testing.mock_ref('slv_flight') %}
-        YEAR    | MONTH 
-        2000    | 1
-        2000    | 0
-        2000    | 13
-        1900    | 12
-        1899    | NULL
-        0       | NULL
+        YEAR    | MONTH | dayOfMonth
+        2000    | 1     | 1
+        1900    | 12    | 31
+
+        2000    | 0     | NULL
+        2000    | 13    | NULL
+        1899    | NULL  | NULL
+        0       | NULL  | NULL
+        2000    | 1     | 32
+        2000    | 1     | 0
     {% endcall %}
 
     {% call dbt_unit_testing.expect() %}
-        YEAR    | MONTH
-        2000    | 1
-        1900    | 12
+        YEAR    | MONTH | dayOfMonth
+        2000    | 1     | 1
+        1900    | 12    | 31
     {% endcall %}
 {% endcall %}
